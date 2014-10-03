7b8495ff6a25951bac53606a0ae9d8417943a9e1
Chris Jackson <darkdragn.cj@gmail.com>
2014-10-03 12:53:57 -0700

    Added MANIFEST.in for pypi. Updated README and setup.py for pypi.

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ VERSION_STR = ".".join([str(x) for x in VERSION])
 setup(
     name='lz4tools',
     version=VERSION_STR,
-    description="LZ4F Bindings for Python",
+    description="LZ4Frame Bindings and tools for Python",
+    license = 'BSD',
     long_description=open('README.rst', 'r').read(),
     author='Christopher Jackson',
     author_email='darkdragn.cj@gmail.com',
@@ -35,6 +36,7 @@ setup(
         ])],
     setup_requires=["nose>=1.0"],
     test_suite = "nose.collector",
+    keywords = ['lz4', 'lz4frame', 'lz4file', 'lz4tar'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
@@ -43,6 +45,5 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
     ],
 )
